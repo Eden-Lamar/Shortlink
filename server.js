042156898a5e10c706c644745341766201c14c25
Eden-Lamr <emmanuelmomodu3@gmail.com>
2023-04-18 10:31:04 +0100

    installed/import dependencies and created server in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// create server and entry point to app
+
+// import dependencies
+const express = require("express");
+const morgan = require("morgan");
+require("colors")
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+// Create server
+app.listen(port, () => {
+	console.log(`\nTHE FORCE IS WITH YOU ON PORT ${port}...\n`.blue.bold)
+})
