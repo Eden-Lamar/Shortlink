e6d92c068cf4d89ff2e962b966ed96eb9ee4f7a5
Eden-Lamr <emmanuelmomodu3@gmail.com>
2023-04-18 11:07:35 +0100

    create router endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,15 @@
 // Import dependencies
 const express = require('express');
 
-
+// Create a router object
 const router = express.Router();
 
+// Endpoint to encode a URL to a short URL
+router.post('/encode',)
+// Endpoint to decode a short URL to its original URL
+router.post('/decode',)
+// Endpoint to get basic statistics of a short URL
+app.get('/statistic/:id',)
 
 
 module.exports = router;
