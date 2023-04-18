8032cce09325c2ff8885ebafe17024b15d4a1632
Eden-Lamr <emmanuelmomodu3@gmail.com>
2023-04-18 11:34:41 +0100

    Import handler functions from the cntroller to router.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,16 @@
 // Import dependencies
 const express = require('express');
+const { encode, decode, linkStat } = require('../controller/shortLinkCtrl');
 
 // Create a router object
 const router = express.Router();
 
 // Endpoint to encode a URL to a short URL
-router.post('/encode',)
+router.post('/encode', encode)
 // Endpoint to decode a short URL to its original URL
-router.post('/decode',)
+router.post('/decode', decode)
 // Endpoint to get basic statistics of a short URL
-app.get('/statistic/:id',)
+router.get('/statistic/:id', linkStat)
 
 
 module.exports = router;
